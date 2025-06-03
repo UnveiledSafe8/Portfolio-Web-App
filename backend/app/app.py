c2ebbe4d8255207bdb3275bcfb0a1ba8b9ae6eb2
Anthony Schurle <115958429+UnveiledSafe8@users.noreply.github.com>
2025-06-03 14:29:27 -0500

    initial commit

---
+++ backend/app/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+app.mount("/frontend/dist", StaticFiles(directory="frontend/dist"), name="frontend-dist")
+
+@app.get("/")
+def index():
+    return FileResponse("frontend/dist/index.html")
