3e146a83d6f7e7cc1e722cd4c6f671726af82e95
Anthony Schurle <115958429+UnveiledSafe8@users.noreply.github.com>
2025-07-09 12:41:12 -0500

    Stricter env control

--- app/backend/database.py
+++ app/backend/database.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine("postgresql+psycopg2://postgres:password@postgres:5432/portfolio_database", echo=True)
+import os
+
+POSTGRES_CONTAINER = os.getenv("POSTGRES_CONTAINER")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+engine = create_engine(f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_CONTAINER}:5432/{POSTGRES_DB}", echo=True)
 
 Base = declarative_base()
 
